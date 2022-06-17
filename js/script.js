d8d7395af60d2d76a63b657cfc2702c0677c003c
Anunciacion Lue <luekely43110@gmail.com>
2022-06-17 21:41:14 +0800

    updated some stuff

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
+const test = document.querySelector('.test');
 
+test.innerHTML =
+	'	Do the dishes<label class="checkbox" for="myCheckboxId5"><input  class="checkbox__input"   type="checkbox"   name="myChecknoxName"      id="myCheckboxId5"  />   <div class="checkbox__box"></div></label>';
