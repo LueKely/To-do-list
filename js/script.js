f04f53d60effb385ca387f3b80b796daceb83eea
Anunciacion Lue <luekely43110@gmail.com>
2022-06-18 22:46:14 +0800

    finally finished the class bp

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
 const test = document.querySelector('.test');
+const test2 = document.querySelector('.test2');
 
-test.innerHTML =
-	'	Do the dishes<label class="checkbox" for="myCheckboxId5"><input  class="checkbox__input"   type="checkbox"   name="myChecknoxName"      id="myCheckboxId5"  />   <div class="checkbox__box"></div></label>';
+
+function jeo ()= 
+
+
+
+class listClass {
+	static count = 0;
+
+	constructor(word) {
+		this.word = word;
+		this.count = listClass.count++;
+	}
+
+	makeList() {
+		return `	<li class="list-item">
+	${this.word}
+	<label class="checkbox" for="myCheckboxId${this.count}">
+		<input
+			class="checkbox__input"
+			type="checkbox"
+			name="myChecknoxName"
+			id="myCheckboxId${this.count}"
+		/>
+		<div class="checkbox__box"></div>
+	</label>`;
+	}
+	logCount() {
+		console.log(this.count);
+	}
+}
+
+const lue = new listClass('nigga');
+const kely = new listClass('joe');
+test.innerHTML = lue.makeList();
+lue.logCount();
+test2.innerHTML = kely.makeList();
+kely.logCount();
